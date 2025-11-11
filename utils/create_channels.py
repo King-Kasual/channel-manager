5ffee005a385b55b4fe30c80d63a452bc3ea6ab4
KingSeth982 <55211035+KingSeth982@users.noreply.github.com>
2025-11-10 22:34:19 -0600

    Edited channel creation
    - Fixed bugs
    - Improved error handling
    - Switched to cloning static channel creation

--- utils/create_channels.py
+++ utils/create_channels.py
@@ -1,6 +1,7 @@
 import discord
 from discord import PermissionOverwrite
 from utils.sql import sql
+from utils.delete_channels import Delete_Dynamic_Channels
 
 
 # Creates new static channel to spawn dynamic channels off of
@@ -17,28 +18,45 @@ async def Create_Static_Channels(
             },
             category=channel,
         )
-
-    except Exception as e:  # pylint: disable=broad-exception-caught
-        response = f"Failed to create channel {name} due to {e}"
-        print(response)
-    else:
-        sql.add_channel(
-            db, "channel_static", new_channel.id, name, new_channel.guild.id
-        )
-        response = f"Channel {name} created successfully"
+    except discord.Forbidden as e:
+        print(f"The bot is missing the permission: {e}")
+        return PermissionError(f"Permission error: {e}")
+    except discord.HTTPException as e:
+        print(f"Creating channel {name} failed due to {e}")
+        return ConnectionError(f"HTTP error: {e}")
+    except discord.InvalidArgument as e:
+        print(f"Invalid argument provided: {e}")
+        return ValueError(f"Invalid argument: {e}")
+    sql.add_channel(db, "channel_static", new_channel.id, name, new_channel.guild.id)
     if debug:
-        print(response)
-    return response
+        print(f"Channel {name} created successfully")
+    return new_channel
 
 
 # Creates new dynamic channel and move the member to the newly created channel
 async def Create_Dynamic_Channels(
     bot, member, channel, db, name, debug=False
-):  # pylint: disable=too-many-arguments,too-many-positional-arguments
-
+):  # pylint: disable=too-many-arguments,too-many-positional-arguments,too-many-return-statements
     try:
-        new_channel = await channel.guild.create_voice_channel(
-            name=name,
+        new_channel = await member.voice.channel.clone(
+            name=name, category=channel.category
+        )
+    except discord.InvalidArgument as e:
+        print(f"Invalid argument provided: {e}")
+        return ValueError(f"Invalid argument: {e}")
+    except discord.Forbidden as e:
+        print(f"The bot is missing the permission: {e}")
+        return PermissionError(f"Permission error: {e}")
+    except discord.HTTPException as e:
+        print(f"Creating channel {name} failed due to {e}")
+        return ConnectionError(f"HTTP error: {e}")
+    sql.add_channel(db, "channel_dynamic", new_channel.id, name, new_channel.guild.id)
+    if debug:
+        print(f"Channel {member.name} created successfully")
+
+    # Set permissions for the new channel
+    try:
+        await new_channel.edit(
             overwrites={
                 bot.user: PermissionOverwrite(
                     manage_permissions=True, move_members=True
@@ -64,21 +82,30 @@ async def Create_Dynamic_Channels(
                     read_messages=True,
                 ),
             },
-            category=channel.category,
-            user_limit=channel.user_limit,
         )
+    except discord.Forbidden as e:
+        print(f"The bot is missing the permission: {e}")
+        await Delete_Dynamic_Channels(new_channel, db, debug=debug)
+        return PermissionError(f"Permission error: {e}")
+    except discord.HTTPException as e:
+        print(f"Setting permissions for channel {name} failed due to {e}")
+        await Delete_Dynamic_Channels(new_channel, db, debug=debug)
+        return ConnectionError(f"HTTP error: {e}")
+    if debug:
+        print(f"Permissions for channel {name} set successfully")
+
+    # Move member to the newly created channel
+    try:
         await member.edit(voice_channel=new_channel)
     except discord.Forbidden as e:
-        response = f"The bot is missing the permission: {e}"
-        print(response)
-    except Exception as e:  # pylint: disable=broad-exception-caught
-        response = f"Failed to create channel {name} due to {e}"
-        print(response)
-    else:
-        sql.add_channel(
-            db, "channel_dynamic", new_channel.id, name, new_channel.guild.id
-        )
-        response = f"Channel {member.name} created successfully"
+        print(f"The bot is missing the permission: {e}")
+        await Delete_Dynamic_Channels(new_channel, db, debug=debug)
+        return PermissionError(f"Permission error: {e}")
+    except discord.HTTPException as e:
+        print(f"Moving member {member.name} to channel {name} failed due to {e}")
+        await Delete_Dynamic_Channels(new_channel, db, debug=debug)
+        return ConnectionError(f"HTTP error: {e}")
     if debug:
-        print(response)
-    return response
+        print(f"Member {member.name} moved to channel {name} successfully")
+
+    return new_channel
